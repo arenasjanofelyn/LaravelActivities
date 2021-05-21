58edd858622308f77f1d3a54066172e656b5d720
arenasjanofelyn <201810488@gordoncollege.edu.ph>
2021-05-22 00:47:21 +0800

    image upload and validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,29 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 
 class PostController extends Controller
 {
-    
-    
-    
-    public function__construct () {
-        $this->middleware('auth')->except(['index', 'store', 'show', 'update']);
-    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-
-
     public function index()
     {
         //
-        $posts = Post::get();
-        return view ('posts.index', compact('posts'));   
+        $posts = Post::get(); 
+        return view('posts.index', compact('posts'));
     }
 
     /**
@@ -48,68 +38,113 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+ 
+        $request->validate([
+            'title' => 'required|max:100',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('img')){
+
+            $filenameWithExt = $request->file('img')->getClientOriginalName();
+
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            
+            $extension = $request->file('img')->getClientOriginalExtension();
+
+            $filenameToStore = $filename.'_'.time().'.'.$extension;
+
+            $path = $request->file('img')->storeAs('public/img', $filenameToStore);
+        } else{
+               
+            $filenameToStore = '';
+        }
+
         $post = new Post();
-        $post->title = $request->title;
-        $post->description = $request->description;
-        $post->save();
+        $post->fill($request->all());
+        $post->img = $filenameToStore;
+
+
+        if ($post->save()){
+            return redirect('/posts')->with('status','Sucessfully save');
+        }
 
         return redirect('/posts');
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
-        $post = Post::find($id);
         return view('posts.show', compact('post'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
-            $post = Post::find($id);
-            return view('posts.edit', compact('post'));
+        return view('posts.edit', compact('post'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+
+        $request->validate([
+            'title' => 'required|max:100',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('img')){
+
+            $filenameWithExt = $request->file('img')->getClientOriginalName();
+
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            
+            $extension = $request->file('img')->getClientOriginalExtension();
+
+            $filenameToStore = $filename.'_'.time().'.'.$extension;
+
+            $path = $request->file('img')->storeAs('public/img', $filenameToStore);
+        } else{
+               
+            $filenameToStore = '';
+        }
+     
         $post = Post::find($id);
-        $post->title = $request->title;
-        $post->description = $request->description;
+        $post->fill($request->all());
+        $post->img = $filenameToStore;
         $post->save();
 
         return redirect('/posts');
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+
         $post = Post::find($id);
         $post->delete();
 
